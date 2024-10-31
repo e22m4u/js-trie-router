c3605112f32236dd396bafa28c22b062ca129b8c
e22m4u <e22m4u@yandex.ru>
2024-10-31 14:25:40 +0300

    fix: missing 'params' key in RequestContext type

--- src/request-context.d.ts
+++ src/request-context.d.ts
@@ -6,6 +6,13 @@ import {ParsedHeaders} from './parsers/index.js';
 import {ServiceContainer} from '@e22m4u/js-service';
 
 /**
+ * Parsed params.
+ */
+export type ParsedParams = {
+  [key: string]: string | undefined;
+};
+
+/**
  * Request context.
  */
 export declare class RequestContext {
@@ -28,6 +35,11 @@ export declare class RequestContext {
    * Query.
    */
   query: ParsedQuery;
+
+  /**
+   * Params.
+   */
+  params: ParsedParams;
 
   /**
    * Headers.
